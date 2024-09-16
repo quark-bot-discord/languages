657b25988bc7213ad4ab207e59961bf600fa0659
Starman <30315137+Starman3787@users.noreply.github.com>
2024-09-16 13:07:30 +0100

    support child properties

--- get.js
+++ get.js
@@ -1,31 +1,72 @@
-const validLanguages = ["en_gb", "en_pr", "en_us", "nl", "pl", "tr", "vi", "es_es"];
+const validLanguages = [
+  "en_gb",
+  "en_pr",
+  "en_us",
+  "nl",
+  "pl",
+  "tr",
+  "vi",
+  "es_es",
+];
+
+const readObject = (obj, cursor = "") => {
+  const cursorPath = cursor.split(".");
+  for (let i = 0; i < cursorPath.length; i++) obj = obj[cursorPath[i]];
+  return obj;
+};
+
+const returnNextProperty = (
+  languagesStringsToUse,
+  fallbackLanguagesStrings,
+  cursor = ""
+) => {
+  return new Proxy(
+    {},
+    {
+      get(target, prop1) {
+        if (prop1 === "then")
+            return returnNextProperty(languagesStringsToUse, fallbackLanguagesStrings);
+        const currentCursor = cursor ? `${cursor}.${prop1}` : prop1;
+        const toReturn = readObject(languagesStringsToUse, currentCursor)
+          ? readObject(languagesStringsToUse, currentCursor)
+          : readObject(fallbackLanguagesStrings.default, currentCursor);
+        switch (typeof toReturn) {
+          case "string":
+            return toReturn;
+          case "object":
+            return returnNextProperty(
+              languagesStringsToUse,
+              fallbackLanguagesStrings,
+              currentCursor
+            );
+          default:
+            return toReturn;
+        }
+      },
+    }
+  );
+};
 
 const languageTypeProxy = (language, type) => {
   return new Proxy(
     {},
     {
-      get(target, prop) {
-        return new Proxy(
-          {},
-          {
-            async get(target, prop1) {
-              const selectedLanguagesStrings = await import(
-                `./bot/${language}/${type}/${prop}.json`,
-                { with: { type: "json" } }
-              ).catch(() => null);
-              const fallbackLanguagesStrings = await import(
-                `./bot/en_us/${type}/${prop}.json`,
-                { with: { type: "json" } }
-              );
-              const languagesStringsToUse = selectedLanguagesStrings?.default
-                ? selectedLanguagesStrings.default
-                : fallbackLanguagesStrings.default;
-              return languagesStringsToUse[prop1]
-                ? languagesStringsToUse[prop1]
-                : fallbackLanguagesStrings.default[prop1];
-            },
-          }
+      async get(target, prop) {
+        const selectedLanguagesStrings = await import(
+          `./bot/${language}/${type}/${prop}.json`,
+          { with: { type: "json" } }
+        ).catch(() => null);
+        const fallbackLanguagesStrings = await import(
+          `./bot/en_us/${type}/${prop}.json`,
+          { with: { type: "json" } }
         );
+        const languagesStringsToUse = selectedLanguagesStrings?.default
+          ? selectedLanguagesStrings.default
+          : fallbackLanguagesStrings.default;
+          return returnNextProperty(
+            languagesStringsToUse,
+            fallbackLanguagesStrings
+          );
       },
     }
   );
