98ccd55e130ac0265e4cc70822fa76a1d4aa4a2c
Starman <30315137+Starman3787@users.noreply.github.com>
2024-09-16 14:58:28 +0100

    Update example.js

--- example.js
+++ example.js
@@ -6,11 +6,13 @@ const language = "en_pr";
 console.log((await languages(language).slash_commands.ping).name);
 
 // existent, should use en_pr
-console.log((await languages(language).standard.textEvents).messageDeleted.title);
+console.log(
+  (await languages(language).standard.textEvents).messageDeleted.title
+);
 
 // non existent in either selected language or fallback language
 try {
-    await languages(language).doesNotExist.abc;
+  await languages(language).doesNotExist.abc;
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
