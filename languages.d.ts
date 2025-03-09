6cde1755f19c32bd4aef02237d148e388261099f
Starman3787 <Starman3787@users.noreply.github.com>
2025-03-09 17:28:33 +0000

    Commit from GitHub Actions (Generate typings)

--- languages.d.ts
+++ languages.d.ts
@@ -422,9 +422,6 @@ export type LanguageStructure = {
   disabled: string;
   none: string;
 }>;
-  thread_update_types: Promise<{
-  none: string;
-}>;
   tags_responses: Promise<{
   "tags-help-description": string;
   "create-success": string;
@@ -448,6 +445,9 @@ export type LanguageStructure = {
   "tag-error-limitreached-0": string;
   "tag-error-limitreached-1": string;
   "tag-createdby": string;
+}>;
+  thread_update_types: Promise<{
+  none: string;
 }>;
   time: Promise<{
   second: string;
@@ -521,20 +521,6 @@ export type LanguageStructure = {
 };
 };
 }>;
-  help: Promise<{
-  name: string;
-  description: string;
-  commandOptions: {
-  overviewOption: {
-  name: string;
-  description: string;
-};
-  serverlogOption: {
-  name: string;
-  description: string;
-};
-};
-}>;
   export: Promise<{
   name: string;
   description: string;
@@ -558,6 +544,20 @@ export type LanguageStructure = {
   name: string;
 };
 };
+};
+};
+}>;
+  help: Promise<{
+  name: string;
+  description: string;
+  commandOptions: {
+  overviewOption: {
+  name: string;
+  description: string;
+};
+  serverlogOption: {
+  name: string;
+  description: string;
 };
 };
 }>;
@@ -596,14 +596,6 @@ export type LanguageStructure = {
   name: string;
   description: string;
 }>;
-  ping: Promise<{
-  name: string;
-  description: string;
-}>;
-  premium: Promise<{
-  name: string;
-  description: string;
-}>;
   mute: Promise<{
   name: string;
   description: string;
@@ -637,7 +629,11 @@ export type LanguageStructure = {
 };
 };
 }>;
-  privacy: Promise<{
+  ping: Promise<{
+  name: string;
+  description: string;
+}>;
+  premium: Promise<{
   name: string;
   description: string;
 }>;
@@ -651,19 +647,9 @@ export type LanguageStructure = {
 };
 };
 }>;
-  reason: Promise<{
+  privacy: Promise<{
   name: string;
   description: string;
-  commandOptions: {
-  caseOption: {
-  name: string;
-  description: string;
-};
-  reasonOption: {
-  name: string;
-  description: string;
-};
-};
 }>;
   serverlog: Promise<{
   name: string;
@@ -723,11 +709,11 @@ export type LanguageStructure = {
 };
 };
 }>;
-  unban: Promise<{
+  reason: Promise<{
   name: string;
   description: string;
   commandOptions: {
-  userOption: {
+  caseOption: {
   name: string;
   description: string;
 };
@@ -798,6 +784,20 @@ export type LanguageStructure = {
   description: string;
 };
   helpOption: {
+  name: string;
+  description: string;
+};
+};
+}>;
+  unban: Promise<{
+  name: string;
+  description: string;
+  commandOptions: {
+  userOption: {
+  name: string;
+  description: string;
+};
+  reasonOption: {
   name: string;
   description: string;
 };
@@ -1361,5 +1361,5 @@ export type LanguageStructure = {
 }>;
 };
 };
-export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "debug" | "help" | "export" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "ping" | "premium" | "mute" | "privacy" | "purge" | "reason" | "serverlog" | "unban" | "tags" | "unmute";
+export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "debug" | "export" | "help" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "premium" | "purge" | "privacy" | "serverlog" | "reason" | "tags" | "unban" | "unmute";
 export type QuarkLanguageCodes = "en_us" | "en_gb" | "tr" | "vi" | "en_pr" | "pl" | "nl" | "es_es" | "it" | "de" | "fr" | "ru" | "el";
