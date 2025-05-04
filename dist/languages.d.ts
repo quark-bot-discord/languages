560bfdc78e0159f2af66f34c7c17a650a0a32796
quark-automations <quark-automations@users.noreply.github.com>
2025-05-04 16:17:50 +0000

    Commit from GitHub Actions (Generate typings)

--- dist/languages.d.ts
+++ dist/languages.d.ts
@@ -29,9 +29,6 @@ export type LanguageStructure = {
   channel_update_types: Promise<{
   none: string;
 }>;
-  emoji_update_types: Promise<{
-  none: string;
-}>;
   command_responses: Promise<{
   disable: string;
   enable: string;
@@ -260,6 +257,9 @@ export type LanguageStructure = {
   "help-modify-tags-description": string;
   "banButton-banning": string;
   "banButton-cancel": string;
+}>;
+  emoji_update_types: Promise<{
+  none: string;
 }>;
   gui_constants: Promise<{
   channelModificationTypes: {
@@ -511,16 +511,6 @@ export type LanguageStructure = {
   name: string;
   description: string;
 }>;
-  debug: Promise<{
-  name: string;
-  description: string;
-  commandOptions: {
-  shareOption: {
-  name: string;
-  description: string;
-};
-};
-}>;
   export: Promise<{
   name: string;
   description: string;
@@ -544,6 +534,16 @@ export type LanguageStructure = {
   name: string;
 };
 };
+};
+};
+}>;
+  debug: Promise<{
+  name: string;
+  description: string;
+  commandOptions: {
+  shareOption: {
+  name: string;
+  description: string;
 };
 };
 }>;
@@ -641,16 +641,6 @@ export type LanguageStructure = {
   name: string;
   description: string;
 }>;
-  purge: Promise<{
-  name: string;
-  description: string;
-  commandOptions: {
-  countOption: {
-  name: string;
-  description: string;
-};
-};
-}>;
   reason: Promise<{
   name: string;
   description: string;
@@ -665,67 +655,11 @@ export type LanguageStructure = {
 };
 };
 }>;
-  tags: Promise<{
+  purge: Promise<{
   name: string;
   description: string;
   commandOptions: {
-  sendOptionTag: {
-  name: string;
-  description: string;
-};
-  sendOptionUser: {
-  name: string;
-  description: string;
-};
-  sendOption: {
-  name: string;
-  description: string;
-};
-  editOptionTag: {
-  name: string;
-  description: string;
-};
-  editOptionText: {
-  name: string;
-  description: string;
-};
-  editOptionColour: {
-  name: string;
-  description: string;
-};
-  editOptionImage: {
-  name: string;
-  description: string;
-};
-  editOption: {
-  name: string;
-  description: string;
-};
-  createOptionTag: {
-  name: string;
-  description: string;
-};
-  createOptionText: {
-  name: string;
-  description: string;
-};
-  createOption: {
-  name: string;
-  description: string;
-};
-  deleteOptionTag: {
-  name: string;
-  description: string;
-};
-  deleteOption: {
-  name: string;
-  description: string;
-};
-  listOption: {
-  name: string;
-  description: string;
-};
-  helpOption: {
+  countOption: {
   name: string;
   description: string;
 };
@@ -798,6 +732,72 @@ export type LanguageStructure = {
   description: string;
 };
   reasonOption: {
+  name: string;
+  description: string;
+};
+};
+}>;
+  tags: Promise<{
+  name: string;
+  description: string;
+  commandOptions: {
+  sendOptionTag: {
+  name: string;
+  description: string;
+};
+  sendOptionUser: {
+  name: string;
+  description: string;
+};
+  sendOption: {
+  name: string;
+  description: string;
+};
+  editOptionTag: {
+  name: string;
+  description: string;
+};
+  editOptionText: {
+  name: string;
+  description: string;
+};
+  editOptionColour: {
+  name: string;
+  description: string;
+};
+  editOptionImage: {
+  name: string;
+  description: string;
+};
+  editOption: {
+  name: string;
+  description: string;
+};
+  createOptionTag: {
+  name: string;
+  description: string;
+};
+  createOptionText: {
+  name: string;
+  description: string;
+};
+  createOption: {
+  name: string;
+  description: string;
+};
+  deleteOptionTag: {
+  name: string;
+  description: string;
+};
+  deleteOption: {
+  name: string;
+  description: string;
+};
+  listOption: {
+  name: string;
+  description: string;
+};
+  helpOption: {
   name: string;
   description: string;
 };
@@ -886,6 +886,26 @@ export type LanguageStructure = {
   unknownChannel: string;
 };
 }>;
+  modlog: Promise<{
+  moderator: string;
+  user: string;
+  reason: string;
+  case: string;
+  noReason: string;
+  noReasonBrief: string;
+  ban: string;
+  unban: string;
+  kick: string;
+  mute: string;
+  unmute: string;
+  timeoutEnds: string;
+  editReason: string;
+  reasonModal: {
+  label: string;
+  placeholder: string;
+  title: string;
+};
+}>;
   generalEvents: Promise<{
   serverModified: {
   title: string;
@@ -914,26 +934,6 @@ export type LanguageStructure = {
   description_withTier: string;
   description_withTier_noUser: string;
   none: string;
-};
-}>;
-  modlog: Promise<{
-  moderator: string;
-  user: string;
-  reason: string;
-  case: string;
-  noReason: string;
-  noReasonBrief: string;
-  ban: string;
-  unban: string;
-  kick: string;
-  mute: string;
-  unmute: string;
-  timeoutEnds: string;
-  editReason: string;
-  reasonModal: {
-  label: string;
-  placeholder: string;
-  title: string;
 };
 }>;
   quarkEvents: Promise<{
@@ -1368,5 +1368,5 @@ export type LanguageStructure = {
 }>;
 };
 };
-export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "debug" | "export" | "help" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "premium" | "privacy" | "purge" | "reason" | "tags" | "serverlog" | "unban" | "unmute";
+export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "export" | "debug" | "help" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "premium" | "privacy" | "reason" | "purge" | "serverlog" | "unban" | "tags" | "unmute";
 export type QuarkLanguageCodes = "en_us" | "en_gb" | "tr" | "vi" | "en_pr" | "pl" | "nl" | "es_es" | "it" | "de" | "fr" | "ru" | "el" | "zh_hant" | "ko";
