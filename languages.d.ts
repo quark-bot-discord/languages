0f9e084623b22a2c7f821e971d817156a57d3470
quark-automations <quark-automations@users.noreply.github.com>
2026-03-22 09:11:41 +0000

    Commit from GitHub Actions (Generate typings)

--- languages.d.ts
+++ languages.d.ts
@@ -29,6 +29,9 @@ export type LanguageStructure = {
   channel_update_types: Promise<{
   none: string;
 }>;
+  emoji_update_types: Promise<{
+  none: string;
+}>;
   command_responses: Promise<{
   disable: string;
   enable: string;
@@ -258,9 +261,6 @@ export type LanguageStructure = {
   "banButton-banning": string;
   "banButton-cancel": string;
 }>;
-  emoji_update_types: Promise<{
-  none: string;
-}>;
   gui_constants: Promise<{
   channelModificationTypes: {
   name: string;
@@ -384,11 +384,6 @@ export type LanguageStructure = {
   "3": string;
   "4": string;
 }>;
-  role_update_types: Promise<{
-  enabled: string;
-  disabled: string;
-  none: string;
-}>;
   permissions: Promise<{
   CREATE_INSTANT_INVITE: string;
   KICK_MEMBERS: string;
@@ -444,6 +439,11 @@ export type LanguageStructure = {
   MANAGE_GUILD_EXPRESSIONS: string;
 }>;
   sticker_update_types: Promise<{
+  none: string;
+}>;
+  role_update_types: Promise<{
+  enabled: string;
+  disabled: string;
   none: string;
 }>;
   tags_responses: Promise<{
@@ -545,20 +545,6 @@ export type LanguageStructure = {
 };
 };
 }>;
-  help: Promise<{
-  name: string;
-  description: string;
-  commandOptions: {
-  overviewOption: {
-  name: string;
-  description: string;
-};
-  serverlogOption: {
-  name: string;
-  description: string;
-};
-};
-}>;
   export: Promise<{
   name: string;
   description: string;
@@ -587,6 +573,20 @@ export type LanguageStructure = {
 }>;
   initialReactor: Promise<{
   name: string;
+}>;
+  help: Promise<{
+  name: string;
+  description: string;
+  commandOptions: {
+  overviewOption: {
+  name: string;
+  description: string;
+};
+  serverlogOption: {
+  name: string;
+  description: string;
+};
+};
 }>;
   invite: Promise<{
   name: string;
@@ -1146,6 +1146,9 @@ export type LanguageStructure = {
   title: string;
   description: string;
 };
+  soundboardSoundUpdated: {
+  title: string;
+};
 }>;
   serverEvents: Promise<{
   members: string;
@@ -1433,5 +1436,5 @@ export type LanguageStructure = {
 }>;
 };
 };
-export type SlashCommandNames = "commands" | "ban" | "config" | "dashboard" | "debug" | "help" | "export" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "privacy" | "premium" | "purge" | "reason" | "serverlog" | "tags" | "unban" | "unmute";
+export type SlashCommandNames = "commands" | "ban" | "config" | "dashboard" | "debug" | "export" | "initialReactor" | "help" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "privacy" | "premium" | "purge" | "reason" | "serverlog" | "tags" | "unban" | "unmute";
 export type QuarkLanguageCodes = "en_us" | "en_gb" | "tr" | "vi" | "en_pr" | "pl" | "nl" | "es_es" | "it" | "de" | "fr" | "ru" | "el" | "zh_hant" | "ko" | "sl" | "ar" | "hu" | "ja";
