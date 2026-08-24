a0983b4a9c4d938629b5536bd5d19a2b5aee36ff
quark-automations <quark-automations@users.noreply.github.com>
2026-08-24 03:55:20 +0000

    Commit from GitHub Actions (Generate typings)

--- dist/languages.d.ts
+++ dist/languages.d.ts
@@ -443,15 +443,18 @@ export type LanguageStructure = {
   MANAGE_EVENTS: string;
   MANAGE_GUILD_EXPRESSIONS: string;
 }>;
-  soundboard_sound_update_types: Promise<{
-  none: string;
-}>;
   role_update_types: Promise<{
   enabled: string;
   disabled: string;
   none: string;
 }>;
+  soundboard_sound_update_types: Promise<{
+  none: string;
+}>;
   sticker_update_types: Promise<{
+  none: string;
+}>;
+  thread_update_types: Promise<{
   none: string;
 }>;
   tags_responses: Promise<{
@@ -477,9 +480,6 @@ export type LanguageStructure = {
   "tag-error-limitreached-0": string;
   "tag-error-limitreached-1": string;
   "tag-createdby": string;
-}>;
-  thread_update_types: Promise<{
-  none: string;
 }>;
   time: Promise<{
   second: string;
@@ -520,6 +520,16 @@ export type LanguageStructure = {
   commands: Promise<{
   name: string;
   description: string;
+}>;
+  debug: Promise<{
+  name: string;
+  description: string;
+  commandOptions: {
+  shareOption: {
+  name: string;
+  description: string;
+};
+};
 }>;
   config: Promise<{
   name: string;
@@ -566,16 +576,6 @@ export type LanguageStructure = {
   name: string;
 };
 };
-};
-};
-}>;
-  debug: Promise<{
-  name: string;
-  description: string;
-  commandOptions: {
-  shareOption: {
-  name: string;
-  description: string;
 };
 };
 }>;
@@ -821,7 +821,7 @@ export type LanguageStructure = {
 };
 };
 }>;
-  unmute: Promise<{
+  unban: Promise<{
   name: string;
   description: string;
   commandOptions: {
@@ -835,7 +835,7 @@ export type LanguageStructure = {
 };
 };
 }>;
-  unban: Promise<{
+  unmute: Promise<{
   name: string;
   description: string;
   commandOptions: {
@@ -1047,6 +1047,7 @@ export type LanguageStructure = {
   unknownAuthor: string;
   contentUnavailable: string;
   hiddenExecutor: string;
+  messageRetention: string;
 };
 }>;
   roleEvents: Promise<{
@@ -1457,5 +1458,5 @@ export type LanguageStructure = {
 }>;
 };
 };
-export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "export" | "debug" | "help" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "premium" | "privacy" | "purge" | "reason" | "serverlog" | "tags" | "unmute" | "unban";
+export type SlashCommandNames = "ban" | "commands" | "debug" | "config" | "dashboard" | "export" | "help" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "premium" | "privacy" | "purge" | "reason" | "serverlog" | "tags" | "unban" | "unmute";
 export type QuarkLanguageCodes = "en_us" | "en_gb" | "tr" | "vi" | "en_pr" | "pl" | "nl" | "es_es" | "it" | "de" | "fr" | "ru" | "el" | "zh_hant" | "ko" | "sl" | "ar" | "hu" | "ja";
