de17e94a6be3a629f79fc26c8363439464dcd126
quark-automations <quark-automations@users.noreply.github.com>
2026-08-25 06:09:14 +0000

    Commit from GitHub Actions (Generate typings)

--- dist/languages.d.ts
+++ dist/languages.d.ts
@@ -454,9 +454,6 @@ export type LanguageStructure = {
   sticker_update_types: Promise<{
   none: string;
 }>;
-  thread_update_types: Promise<{
-  none: string;
-}>;
   tags_responses: Promise<{
   "tags-help-description": string;
   "create-success": string;
@@ -480,6 +477,9 @@ export type LanguageStructure = {
   "tag-error-limitreached-0": string;
   "tag-error-limitreached-1": string;
   "tag-createdby": string;
+}>;
+  thread_update_types: Promise<{
+  none: string;
 }>;
   time: Promise<{
   second: string;
@@ -600,16 +600,6 @@ export type LanguageStructure = {
   name: string;
   description: string;
 }>;
-  language: Promise<{
-  name: string;
-  description: string;
-  commandOptions: {
-  languageOption: {
-  name: string;
-  description: string;
-};
-};
-}>;
   kick: Promise<{
   name: string;
   description: string;
@@ -619,6 +609,16 @@ export type LanguageStructure = {
   description: string;
 };
   reasonOption: {
+  name: string;
+  description: string;
+};
+};
+}>;
+  language: Promise<{
+  name: string;
+  description: string;
+  commandOptions: {
+  languageOption: {
   name: string;
   description: string;
 };
@@ -1182,6 +1182,7 @@ export type LanguageStructure = {
   exemptRoles: string;
   exemptChannels: string;
   conditions: string;
+  actions: string;
 };
   autoModerationRule: {
   trigger: {
@@ -1489,5 +1490,5 @@ export type LanguageStructure = {
 }>;
 };
 };
-export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "debug" | "export" | "help" | "initialReactor" | "invite" | "language" | "kick" | "logging" | "mute" | "ping" | "premium" | "privacy" | "purge" | "reason" | "serverlog" | "tags" | "unban" | "unmute";
+export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "debug" | "export" | "help" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "premium" | "privacy" | "purge" | "reason" | "serverlog" | "tags" | "unban" | "unmute";
 export type QuarkLanguageCodes = "en_us" | "en_gb" | "tr" | "vi" | "en_pr" | "pl" | "nl" | "es_es" | "it" | "de" | "fr" | "ru" | "el" | "zh_hant" | "ko" | "sl" | "ar" | "hu" | "ja";
