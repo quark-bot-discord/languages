e177de7d028182dd14c9711778a48968146caa4a
quark-automations <quark-automations@users.noreply.github.com>
2026-09-17 15:26:27 +0000

    Commit from GitHub Actions (Generate typings)

--- dist/languages.d.ts
+++ dist/languages.d.ts
@@ -29,6 +29,9 @@ export type LanguageStructure = {
   channel_update_types: Promise<{
   none: string;
 }>;
+  emoji_update_types: Promise<{
+  none: string;
+}>;
   command_responses: Promise<{
   disable: string;
   enable: string;
@@ -257,9 +260,7 @@ export type LanguageStructure = {
   "help-modify-tags-description": string;
   "banButton-banning": string;
   "banButton-cancel": string;
-}>;
-  emoji_update_types: Promise<{
-  none: string;
+  "setserverlog-live-title": string;
 }>;
   gui_constants: Promise<{
   channelModificationTypes: {
@@ -661,6 +662,10 @@ export type LanguageStructure = {
 };
 };
 }>;
+  ping: Promise<{
+  name: string;
+  description: string;
+}>;
   premium: Promise<{
   name: string;
   description: string;
@@ -669,9 +674,15 @@ export type LanguageStructure = {
   name: string;
   description: string;
 }>;
-  ping: Promise<{
+  purge: Promise<{
   name: string;
   description: string;
+  commandOptions: {
+  countOption: {
+  name: string;
+  description: string;
+};
+};
 }>;
   reason: Promise<{
   name: string;
@@ -682,16 +693,6 @@ export type LanguageStructure = {
   description: string;
 };
   reasonOption: {
-  name: string;
-  description: string;
-};
-};
-}>;
-  purge: Promise<{
-  name: string;
-  description: string;
-  commandOptions: {
-  countOption: {
   name: string;
   description: string;
 };
@@ -821,6 +822,16 @@ export type LanguageStructure = {
 };
 };
 }>;
+  troubleshoot: Promise<{
+  name: string;
+  description: string;
+  commandOptions: {
+  shareOption: {
+  name: string;
+  description: string;
+};
+};
+}>;
   unban: Promise<{
   name: string;
   description: string;
@@ -852,16 +863,6 @@ export type LanguageStructure = {
   vote: Promise<{
   name: string;
   description: string;
-}>;
-  troubleshoot: Promise<{
-  name: string;
-  description: string;
-  commandOptions: {
-  shareOption: {
-  name: string;
-  description: string;
-};
-};
 }>;
 };
   standard: {
@@ -1811,5 +1812,5 @@ export type LanguageStructure = {
 }>;
 };
 };
-export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "debug" | "export" | "help" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "premium" | "privacy" | "ping" | "reason" | "purge" | "serverlog" | "tags" | "unban" | "unmute" | "vote" | "troubleshoot";
+export type SlashCommandNames = "ban" | "commands" | "config" | "dashboard" | "debug" | "export" | "help" | "initialReactor" | "invite" | "kick" | "language" | "logging" | "mute" | "ping" | "premium" | "privacy" | "purge" | "reason" | "serverlog" | "tags" | "troubleshoot" | "unban" | "unmute" | "vote";
 export type QuarkLanguageCodes = "en_us" | "en_gb" | "tr" | "vi" | "en_pr" | "pl" | "nl" | "es_es" | "it" | "de" | "fr" | "ru" | "el" | "zh_hant" | "ko" | "sl" | "ar" | "hu" | "ja";
